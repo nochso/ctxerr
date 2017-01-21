f9ccaf05d7ae7df72ea5b44f92d927c749c2fd34
Marcel Voigt <mv@noch.so>
2017-01-21 22:19:48 +0100

    Fix InvalidInt test some more

--- parser_test.go
+++ parser_test.go
@@ -52,7 +52,7 @@ func TestParse_InvalidInt(t *testing.T) {
 	if err == nil {
 		t.Error("expected error, got nil")
 	}
-	prefix := "strconv.Atoi"
+	prefix := "strconv"
 	if !strings.HasPrefix(err.Error(), prefix) {
 		t.Errorf("expected error to have prefix %#v, got %#v", prefix, err.Error())
 	}
